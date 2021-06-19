d32d3acea46827ec2fc674b80e457d091424e8df
Ran <cs.pangge@gmail.com>
2021-06-19 13:55:17 +1000

    update

---
+++ Utilities/tools.go
@@ -0,0 +1,22 @@
+package Utilities
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+// GetEnv use godot package to load/read the .env file and
+// return the value of the key
+func GetEnv(key string) string {
+	// load .env file
+	err := godotenv.Load(".env")
+	CheckErr(err)
+	return os.Getenv(key)
+}
+
+func CheckErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module restful-api-kit
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/joho/godotenv v1.3.0
+)
